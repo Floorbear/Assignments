1012fdad715aa7b20f7fd97a253a50ae54adfea8
floorbear2723 <floorbear2723@gmail.com>
2021-11-08 20:11:02 +0900

    2번까지 완료

--- Algorithm/Assignment3/Assignment3.cpp
+++ Algorithm/Assignment3/Assignment3.cpp
@@ -1,20 +1,83 @@
-﻿// Assignment3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
+using namespace std;
+
+
+// -------------------------------------------------------------  선  언 -----------------------------------------------------------------------------------------------
+//5개 이하 리스트에서 Insertion Sort를 이용해서 원하는 Index의 key값을 찾는 함수
+int InsertionSort(vector<int> list, int want_index);
+
+//메인 알고리즘
+int LinearTimeSelection(vector<int> list, int want_index, int split_size);
+
+// ----------------------------------- 정 의 --------------------------------------------------------------------------------------------------------------------------------
+
+int InsertionSort(vector<int> list, int want_index)
+{
+    if (list.size() == 1) //list의 사이즈가 1이면 그냥 반환
+    {
+        return list.front();
+    }
+
+
+    for (vector<int>::iterator i = list.begin() + 1; i < list.end(); i++)
+    {
+        auto temp = i;
+        while (temp != list.begin() && *temp < *(temp - 1))
+        {
+            iter_swap(temp, temp - 1);
+            temp--;
+        }
+    }
+    return list[want_index - 1];
+}
+
+int LinearTimeSelection(vector<int> list, int want_index, int split_size)
+{
+    //elements의 숫자가 split_size 보다 작으면 InsertionSort를 이용해 원하는 element를 찾는다
+    if (list.size() <= split_size)
+    {
+        return InsertionSort(list, want_index);
+    }
+
+    // n/split_size 만큼의 그룹을 생성한다
+    int group_count;
+    if (list.size() % split_size == 0)
+    {
+        group_count = list.size() / split_size;
+    }
+    else
+    {
+        group_count = list.size() / split_size + 1;
+    }
+    vector<int>* group = new vector<int>[group_count];
+
+    //각 그룹에 split_size만큼 element를 넣어준다
+    for (int i = 0; i < group_count; i++)
+    {
+        for (int j = 0; j < split_size; j++)
+        {
+            if (!list.empty())
+            {
+                group[i].push_back(list.front());
+                list.erase(list.begin());
+            }
+            else
+                break;
+            
+        }
+    }
+    cout << "hi" << endl;
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> new_list = { 3,7,8,2,5,6 };
+    cout << LinearTimeSelection(new_list,4,2);
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
