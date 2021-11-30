c4ff2d08a6b6e304f81cb115131f018c90fab170
floorbear2723 <floorbear2723@gmail.com>
2021-11-30 19:31:54 +0900

    알고리즘 과제4 Finish

---
+++ Algorithm/Assignment4/Assignment4.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+#define INFINITY 1000
+
+using namespace std;
+
+//minimum depth value를 저장할 변수
+vector<int> Output[50];
+
+//horizontal에 대한 minimum depth value를 리턴하는 함수
+vector<int> FindDepthValue(vector<int>(&depth_table)[5]);
+
+
+int main()
+{
+    //동일한 시험군을 이용해야하므로 랜덤 시드를 고정
+    std:mt19937 gen(3631);
+    //분포 정의 >> 0부터 10까지 균등한 확률로 정의
+    std::uniform_int_distribution<int> dis(0, 10);
+    //each row에 있는 5*100테이블 [vertical][depth_value], column value
+    vector<int> depth_table[50][5];
+    
+    //depth_table 초기화
+    for (int i= 0; i < 50;i++) //vertical
+    {
+
+        for (int j = 0; j < 5; j++) //depth_value
+        {
+            for (int k = 0; k < 100; k++) //horizontal
+            {
+                //0~10 사이의 난수 생성
+                int num = dis(gen);
+                //난수를 넣어준다
+                depth_table[i][j].push_back(num);
+            }
+        }
+    }
+
+    //Minimum Depth Value를 찾는다
+    for (int i = 0; i < 50; i++)
+    {
+        //각줄의 table에 대해 Find Depth Value 진행하고 결과값을 new_List에 저장
+        vector<int> new_List = FindDepthValue(depth_table[i]);
+
+        //new_List에 저장된 값을 Output에 복사
+        for (int j : new_List)
+        {
+            Output[i].push_back(j);
+        }
+    }
+
+    //Output 출력
+    for (int i = 0; i < 50; i++)
+    {
+        for (int j : Output[i])
+        {
+            cout << j << " ";
+        }
+        printf("\n\n");
+    }
+}
+
+vector<int> FindDepthValue(vector<int>(&depth_table)[5])
+{
+    //return 값을 저장한 벡터
+    vector<int> new_list;
+
+    //horizontal
+    for (int i = 0; i < 100; i++)
+    {
+        //i=0일때 예외처리 >> 첫 열에서 가장작은 depth value를 저장한다
+        if (i == 0)
+        {
+            //가장 작은 Index를 저장할 변수
+            int current_depth_value = 0;
+            //가장 작은 Index의 Cost을 저장할 변수
+            int cuuurent_cost = INFINITY;
+            //vertical
+            for (int j = 0; j < 5; j++)
+            {
+                int cost = depth_table[j][0];
+                //현재 코스트 총합이 더 작다면 Update
+                if (cuuurent_cost > cost)
+                {
+                    current_depth_value = j;
+                    cuuurent_cost = cost;
+                }
+            }
+            //가장 작은 depth_value를 newlist에 넣어준다
+            new_list.push_back(current_depth_value);
+            continue;
+        }
+
+        int current_cost = INFINITY;
+        int current_depth_value = 0;
+        int prev_depth_value = new_list.back();
+        int prev_cost = depth_table[prev_depth_value][i - 1];
+
+        //vertical
+        for (int j = 0; j < 5; j++)
+        {
+            //smoothness = 이전 depth value - 현 depth value의 절댓값
+            int smoothness = abs(j - prev_depth_value);
+            //이전 코스트 + 현재 코스트 + smoothness*감마
+            int cost = prev_cost + smoothness * 5 + depth_table[j][i];
+            //현재 코스트 총합이 더 작다면 Update
+            if (current_cost > cost)
+            {
+                current_cost = cost;
+                current_depth_value = j;
+            }
+        }
+        //가장 작은 depth_value를 newlist에 넣어준다
+        new_list.push_back(current_depth_value);
+
+    }
+    return new_list;
+}
---
+++ Network/FinalProject/FinalProject.cpp
@@ -0,0 +1,86 @@
+﻿#define HAVE_STRUCT_TIMESPEC
+#define _CRT_SECURE_NO_WARNINGS
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+/*void* thread_main(void* arg);
+void* thread_main_1(void* arg);
+
+pthread_t t_id;
+pthread_t t_id_2;
+int main(int argc, char* argv[])
+{
+	
+	int thread_param = 5; //쓰레드 메인함수에 전달할 매개변수
+
+
+	void* thr_ret; //쓰래드가 리턴하는 값
+	void* thr_ret_2;
+
+	if (pthread_create(&t_id, NULL, thread_main, &thread_param) != 0)
+	{
+		printf("pthread_creat error");
+		return -1;
+	}
+	pthread_detach(t_id);
+	int a = pthread_create(&t_id, NULL, thread_main_1, &thread_param);
+	printf("%d", a);
+
+	pthread_join(t_id, &thr_ret);
+	
+	
+
+}
+
+//메인 쓰레드 함수
+void* thread_main(void* arg)
+{
+	int i;
+	i = *((int*)arg);
+
+	printf("쓰레드 1 작동중 \n");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("쓰레드 1 : 호출\n");
+		Sleep(1000);
+	}
+	char* msg = (char*)malloc(sizeof(char) * 50);
+	strcpy(msg, "쓰레드 1 종료");
+	printf("%s", msg);
+
+	return (void*)msg;
+}
+
+void* thread_main_1(void* arg)
+{
+	int i;
+	i = *((int*)arg);
+
+	printf("쓰레드2 작동중 \n");
+	for (int i = 0; i < 3; i++)
+	{
+		printf("쓰레드 2 : 호출\n");
+		Sleep(1000);
+	}
+	Sleep(1000);
+	char* msg = (char*)malloc(sizeof(char) * 50);
+	strcpy(msg, "쓰레드2 종료");
+	printf("%s", msg);
+	return (void*)msg;
+}*/
+int main(int argc, char* argv[])
+{
+
+	char str[] = "Block|D|Mask.";    //구분할 문자열
+	char* ptr = strtok(str, "|");    //첫번째 strtok 사용.
+	strcpy(str, ptr);
+	printf("%s\n", str);
+	ptr = strtok(NULL, "|");
+	strcpy(str, ptr);
+	printf("%s", str);
+
+
+}
