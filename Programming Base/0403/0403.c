31bb837c837217c8400e72a4976d9e2123cfa331
Floorbear <floorbear2723@gmail.com>
2023-04-03 15:44:46 +0900

    이동원 : 커밋

---
+++ Programming Base/0403/0403.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+
+
+
+int main()
+{
+	int iDiceEyes1 = 0;
+	int iDiceEyes2 = 0;
+	int iDiceEyes3 = 0;
+	int iSameCount = 0;
+	int iMostEyes = 0;
+	int iSameEyes = 0;
+	int iMoney = 0;
+
+	printf("주사위 눈 3개를 입력해 주세요 : \n");
+	scanf_s("%d %d %d", &iDiceEyes1, &iDiceEyes2, &iDiceEyes3);
+
+	iMostEyes = iDiceEyes1;
+	if (iDiceEyes2 > iMostEyes)
+	{
+		iMostEyes = iDiceEyes2;
+	}
+	if (iDiceEyes3 > iMostEyes)
+	{
+		iMostEyes = iDiceEyes3;
+	}
+
+	if (iDiceEyes1 == iDiceEyes2)
+	{
+		iSameCount = 2;
+		iSameEyes = iDiceEyes1;
+		if (iDiceEyes1 == iDiceEyes3)
+		{
+			iSameCount = 3;
+		}
+	}
+	else if (iDiceEyes1 == iDiceEyes3)
+	{
+		iSameCount = 2;
+		iSameEyes = iDiceEyes1;
+	}
+	else if (iDiceEyes2 == iDiceEyes3)
+	{
+		iSameCount = 2;
+		iSameEyes = iDiceEyes2;
+	}
+
+	if (iSameCount == 0)
+	{
+		iMoney = iMostEyes * 100;
+	}
+	else if (iSameCount == 2)
+	{
+		iMoney = 1000 + iSameEyes * 100;
+	}
+	else if (iSameCount == 3)
+	{
+		iMoney = 10000 + iSameEyes * 1000;
+	}
+
+	printf("상금 : %d", iMoney);
+}
---
+++ Programming Base/MyAccount/MyAccount.c
@@ -0,0 +1,92 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int iOldID = 115;
+	int iOldMoney = 70;
+
+	int iNewID = -1;
+	int iNewMoney = -1;
+
+	int iInput = 0;
+
+	printf("---- Menu ---- \n");
+	printf("1.	계좌 계설 \n");
+	printf("2.	입금 \n");
+	printf("3.	출금 \n");
+	printf("4.	계좌정보 전체 출력 \n");
+	printf("5.	프로그램 종료 \n");
+	printf("선택 : ");
+	scanf_s("%d", &iInput);
+
+	if (iInput == 1)
+	{
+		printf("[계좌개설]\n");
+		printf("계좌ID : ");
+		scanf_s("%d", &iNewID);
+		printf("입금액 : ");
+		scanf_s("%d", &iNewMoney);
+		if (iNewID == iOldID)
+		{
+			printf("**계좌 개설 실패**\n이미 존재하는 아이디입니다.\n");
+			return;
+		}
+		printf("**계좌 개설 성공**\n");
+		printf("계좌ID : %d\n",iNewID);
+		printf("입금액 : %d\n",iNewMoney);
+		return;
+	}
+	else if (iInput == 2)
+	{
+		printf("[입 금]\n");
+		printf("계좌ID : ");
+		scanf_s("%d", &iNewID);
+		printf("입금액 : ");
+		scanf_s("%d", &iNewMoney);
+		if (iOldID != iNewID)
+		{
+			printf("**입금 실패**\n계좌ID가 존재하지 않습니다.\n");
+			return;
+		}
+		iOldMoney += iNewMoney;
+		printf("**입금 성공**\n");
+		printf("계좌ID : %d\n", iNewID);
+		printf("입금액 : %d\n", iOldMoney);
+		return;
+	}
+	else if (iInput == 3)
+	{
+		printf("[출 금]\n");
+		printf("계좌ID : ");
+		scanf_s("%d", &iNewID);
+		printf("출금액 : ");
+		scanf_s("%d", &iNewMoney);
+		if (iOldID != iNewID)
+		{
+			printf("**출금 실패**\n계좌ID가 존재하지 않습니다.\n");
+			return;
+		}
+		if (iNewMoney > iOldMoney)
+		{
+			printf("**출금 실패**\n잔액이 부족합니다.\n");
+			return;
+		}
+		iOldMoney -= iNewMoney;
+		printf("**출금 성공**\n");
+		printf("계좌ID : %d\n", iNewID);
+		printf("출금액 : %d\n", iOldMoney);
+		return;
+	}
+	else if (iInput == 4)
+	{
+		printf("계좌ID : %d\n", iOldID);
+		printf("입금액 : %d\n", iOldMoney);
+		return;
+	}
+	else if (iInput == 5)
+	{
+		printf("프로그램을 종료합니다\n");
+		return;
+	}
+}
+
