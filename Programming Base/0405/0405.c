ea0a8968717a161bce0b8557cb21bde9dfd1fcc4
Floorbear <floorbear2723@gmail.com>
2023-04-08 22:14:27 +0900

    이동원 : pud

---
+++ Programming Base/0405/0405.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+//학과 멀티미디어공학과
+//이름 이동원
+//학번 2019113931
+int main()
+{
+	//int iValue = 0;
+	//scanf_s("%d", &iValue);
+	//for (int i = 1; i < 10; i++)
+	//{
+	//	int iResult = iValue * i;
+	//	printf("%d * %d = %d\n", i, iValue, iResult);
+	//}
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j <= i)
+			{
+				printf("ㅇ ");
+			}
+		}
+		printf("\n");
+	}
+}
+
+
